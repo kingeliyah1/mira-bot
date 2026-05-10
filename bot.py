0ecc62d2a744f52ca53b72046085f07336bc3c82
Mira AI <musanurudeenking@gmail.com>
2026-05-10 22:39:43 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = "8733211848:AAFPyyCOUFOhXt8SD32KRUKnoA508DHB7GY
+TOKEN = "8733211848:AA...FPyyCOUFOhXt8SD32KRUKnoA508DHB7GY"
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
